371b75a9b71b5dc0c6050521beb19403a4b17ff2
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-06-29 11:38:58 +0600

    Variables

---
+++ FirstPythonProgram.py
@@ -0,0 +1,24 @@
+print("My name is Shajalal")
+print("I ma Student")
+
+
+print("My name is Shajalal","I ma Student")
+
+print(23)
+print(33+12)
+
+
+# Variables
+name = "Md.Shajalal"
+age = 23
+Price = 34.56
+
+age2 = age
+
+print("my name is :",name)
+print("my age:",age2)
+
+print(type(name))
+print(type(age))
+print(type(Price))
+ 
