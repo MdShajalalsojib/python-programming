3d501bda682345dc9800591a6224338d8a694be1
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-07-01 23:37:44 +0600

    Sutdent marks if elif and else

---
+++ Exam_Marks.py
@@ -0,0 +1,18 @@
+marks = input("marks : ")
+if(marks >= 90):
+    print("A+")
+elif(marks >= 80 and marks < 90):
+    print("A")
+elif(marks >= 70 and marks < 80):
+    print("A-")
+elif(marks >= 60 and marks < 70):
+    print("B+")
+elif(marks >= 50 and marks < 60):
+    print("B")
+elif(marks >= 40 and marks < 50):
+    print("B-")
+elif(marks >= 33 and marks < 40):
+    print("C")
+else:
+    print("F")           
+
