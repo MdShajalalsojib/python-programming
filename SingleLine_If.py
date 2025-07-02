b383cfca1ed222e000b4df7c3e1520f0cca81a7b
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-07-02 11:48:29 +0600

    Single Line if / Ternary Operator

--- SingleLine_If.py
+++ SingleLine_If.py
@@ -3,3 +3,8 @@
 food = input("Foods Names : ")
 eat = "Yes" if food == "cake" else "no"
 print(eat)
+
+
+# <stt1>if<condition>else<stt2>
+food = input("Food : ")
+print("Sweet") if food =="cake" or food == "jalebi" else print("Not Sweet")
